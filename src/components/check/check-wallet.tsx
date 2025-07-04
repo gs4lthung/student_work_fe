ce80631a65f142733c1549f814f47e24bce16923
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2025-07-04 08:04:15 +0700

    feat: add interview invitation status badge in AppliedJobsPage; improve wallet check logic in CheckWallet component

--- src/components/check/check-wallet.tsx
+++ src/components/check/check-wallet.tsx
@@ -2,17 +2,16 @@
 
 import { getWalletByUserId } from "@/api/wallet-api";
 import { useUserStore } from "@/stores/user-store";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
 export default function CheckWallet() {
   const { user } = useUserStore();
-  const [isChecked, setIsChecked] = React.useState(false);
   useEffect(() => {
     async function fetchWallet() {
-      if (!user || isChecked || user.role === "Student") return;
-      
+      if (!user || user.role === "Student") return;
+
       try {
-        console.log("check wallet")
+        console.log("check wallet");
         const wallet = await getWalletByUserId(user.userId!);
         if (wallet) {
           useUserStore.getState().setUser({
@@ -20,13 +19,12 @@ export default function CheckWallet() {
             walletID: wallet.walletID,
             walletBalance: wallet.balance,
           });
-          setIsChecked(true);
         }
       } catch (error) {
         console.error("Failed to fetch wallet:", error);
       }
     }
     fetchWallet();
-  }, [isChecked, user]);
-  return <></>;
+  }, [user]);
+  return null;
 }
